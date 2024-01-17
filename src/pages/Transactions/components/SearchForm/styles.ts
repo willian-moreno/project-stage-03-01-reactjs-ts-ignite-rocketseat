c5f8b485d75cc9de88b13ddef977ba96626823f9
willianam <willianam@uninove.br>
2024-01-17 11:50:48 -0300

    feat: Componente SearchForm

---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+
+    background-color: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+
+    margin-bottom: 1.5rem;
+    padding: 1rem;
+
+    border: 0;
+    border-radius: 6px;
+
+    &::placeholder {
+      color: ${(props) => props.theme['gray-500']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    background-color: transparent;
+    color: ${(props) => props.theme['green-300']};
+
+    border: 0;
+    border: 1px solid ${(props) => props.theme['green-300']};
+    border-radius: 6px;
+
+    height: fit-content;
+    padding: 0.875rem 2rem;
+
+    font-weight: bold;
+
+    cursor: pointer;
+
+    transition:
+      background-color 0.15s,
+      color 0.15s,
+      border-color 0.15s;
+
+    &:hover {
+      background-color: ${(props) => props.theme['green-500']};
+      border-color: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+    }
+  }
+`
