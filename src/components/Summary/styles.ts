0e2a7fb0351856eac4f34b9d8ab357fab41247a4
willianam <willianam@uninove.br>
2024-01-17 10:55:38 -0300

    feat: Componente Summary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components'
+
+export const SummaryContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  width: 100%;
+  max-width: 1120px;
+
+  margin: -5rem auto;
+  padding: 0 1.5rem;
+
+  overflow-x: auto;
+`
+interface SummaryCardProps {
+  variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background-color: ${(props) => props.theme['gray-600']};
+
+  padding: 2rem;
+
+  border-radius: 6px;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${(props) => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+
+    margin-top: 1rem;
+
+    font-size: 2rem;
+  }
+
+  ${(props) =>
+    props.variant === 'green' &&
+    css`
+      background-color: ${props.theme['green-700']};
+    `}
+`
